fc8fd96c5fed6c1098687b23e7950ad1663a981c
DrBAXA <drbaxa@hotmail.com>
2015-07-25 02:13:20 +0300

    create table functions

--- table.js
+++ table.js
@@ -1,5 +1,7 @@
 var Table = (function(){
-    function Table(){}
+    function Table(){
+
+    }
 
     /*
     returns cell by that index
@@ -32,18 +34,17 @@ var Table = (function(){
         return {"i": aI, "j": aJ};
     };
 
-    Table.prototype.markLine = function(cell){
+    /*
+    Mark given line and invoke it's in Matrix
+     */
+    Table.prototype.drawLine = function(cell){
 
         var cellIndex = this.getCellIndex(cell);
 
-        /*
-        cell is element, not a  line
-         */
+        //cell is element, not a  line
         if(cellIndex.i%2 == 0 && cellIndex.j%2 == 0) throw "IllegalArgumentException";
 
-        /*
-        cell is small appendix cross lines
-         */
+        //cell is small appendix cross lines
         if(cellIndex.i%2 == 1 && cellIndex.j%2 == 1) throw "IllegalArgumentException";
 
         jQuery(cell).addClass('marked');
@@ -51,17 +52,56 @@ var Table = (function(){
         var elements = [];
 
         if(cellIndex.i%2 == 0){//element is vertical line
-            elements[0] = this.getArrayIndex({"i": cellIndex.i, "j": cellIndex.j - 1});
-            elements[1] = this.getArrayIndex({"i": cellIndex.i, "j": cellIndex.j + 1});
-        }else if(cellIndex.j%2 == 0){
-            elements[0] = this.getArrayIndex({"i": cellIndex.i - 1, "j": cellIndex.j});
-            elements[1] = this.getArrayIndex({"i": cellIndex.i + 1, "j": cellIndex.j});
+            elements[0] = this.getArrayIndex(cellIndex.i, cellIndex.j - 1);
+            elements[1] = this.getArrayIndex(cellIndex.i, cellIndex.j + 1);
+        }else if(cellIndex.j%2 == 0){//element is horizontal line
+            elements[0] = this.getArrayIndex(cellIndex.i - 1, cellIndex.j);
+            elements[1] = this.getArrayIndex(cellIndex.i + 1, cellIndex.j);
         }
+        try{
+            Matrix.drawLine(elements);
+        }catch (e){
+            console.log("Matrix is not defined");
+        }
+
+        return elements;
 
     };
 
+    Table.prototype.createTable = function(size){
+        var table = jQuery('#table');
 
+        for(var i = 0; i < size; i++){
+            var tr = createRow(i);
+            for(var j = 0; j < size; j++){
+                var td = createColumn(j);
+                jQuery(tr).append(td)
+            }
+        }
+    };
 
+    function createColumn(index){
+        var td = jQuery('<td></td>');
+        if(index%2 == 0){
+            jQuery(td).addClass('wide');
+        } else{
+            jQuery(td).addClass('narrow');
+            jQuery(td).bind('click', function(element){
+                Table.drawLine.call(table, element);
+            })
+        }
+        return td;
+    }
+
+    function createRow(index){
+        var tr = jQuery('<tr></tr>');
+        if(index%2 == 0){
+            jQuery(tr).addClass('tall');
+        } else{
+            jQuery(tr).addClass('low');
+        }
+        return tr;
+    }
 
     return new Table();
 })();
