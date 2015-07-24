99df424911a0a04d9c6c78210c1311074a63d9f6
DrBAXA <drbaxa@hotmail.com>
2015-07-25 00:48:22 +0300

    cell index functions

--- table.js
+++ table.js
@@ -1,9 +1,38 @@
 var Table = (function(){
     function Table(){}
 
+    /*
+    returns cell by that index
+     */
     Table.prototype.getViewCell = function(i,j) {
         return jQuery('#table tr:eq(' + i + ') td:eq(' + j + ')')
     };
+
+    /*
+    returns index by given cell
+     */
+    Table.prototype.getCellIndex = function(cell){
+        var aI = jQuery(cell).closest('tr').index();
+        var aJ = jQuery(cell).index();
+
+        return {"i": aI, "j": aJ};
+    };
+
+    /*
+    Convert table index to array index
+     */
+    Table.prototype.getArrayIndex = function(i,j){
+        var aI, aJ;
+
+        if(i%2 == 1 || j%2 == 1) return undefined;
+
+        aI = i/2;
+        aJ = j/2;
+
+        return {"i": aI, "j": aJ};
+    };
+
+
 
 
     return new Table();
