ba8ff8998d870291efc7888e2d4afff3b61b4adc
MaksimToll <tolstik.maksim@yandex.ua>
2015-09-20 12:26:08 +0300

    remove init buttom, add onload use angular

--- view/js/registrationCtrl.js
+++ view/js/registrationCtrl.js
@@ -1,22 +1,26 @@
 /**
  * Created by maks on 13.09.2015.
  */
-angular.module('myApp', []).controller('regCtrl', function($scope, $http) {
+angular.module('myApp', []).controller('regCtrl', function ($scope, $http) {
     $scope.firstName = "John",
         $scope.lastName = "Doe",
-        $scope.myData={};
+        $scope.myData = {};
 
-        $scope.myData.doClick = function(item, event){
-            var user = {name: $scope.username};
-            var responsePromise = $http.post("/register",user);
-            responsePromise.success(function(data, status, headers, config) {
-                //$scope.myData.fromServer = data.title();
-            });
-            responsePromise.error(function(data, status, headers, config) {
-                Console.log("AJAX failed!");
-            });
-        }
-        $scope.fullName = function() {
-            return $scope.firstName + " " + $scope.lastName;
-        }
+    $scope.myData.doClick = function (item, event) {
+        var user = {name: $scope.username};
+        var responsePromise = $http.post("/register", user);
+        responsePromise.success(function (data, status, headers, config) {
+            //$scope.myData.fromServer = data.title();
+        });
+        responsePromise.error(function (data, status, headers, config) {
+            Console.log("AJAX failed!");
+        });
+    }
+    $scope.fullName = function () {
+        return $scope.firstName + " " + $scope.lastName;
+    }
+    $scope.showRegWindow = function (){
+            $('#myModal').modal('show');
+    }
+
 });
